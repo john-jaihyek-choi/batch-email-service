c59c61aba3d08072b9eaccca71a3813b34d13667
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-01-15 02:18:12 -0600

    format and filter target method implementation roughdraft complete

--- cdk/lambdas/python/functions/send_batch_email_event/lambda_function.py
+++ cdk/lambdas/python/functions/send_batch_email_event/lambda_function.py
@@ -1,8 +1,12 @@
 import json
 import os
 import logging
+import urllib.parse
 import boto3
-from typing import Dict, Any
+import csv
+import io
+from datetime import datetime, timezone
+from typing import Dict, Any, List
 from http import HTTPStatus
 from dotenv import load_dotenv
 
@@ -13,86 +17,47 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-sqs = boto3.client("sqs", os.getenv("AWS_DEFAULT_REGION", "us-east-2"))
+aws_region = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
 
-def lambda_handler(event, context):
-    # Example S3 Put Event
-    # {
-    #   "Records": [
-    #     {
-    #       "eventVersion": "2.0",
-    #       "eventSource": "aws:s3",
-    #       "awsRegion": "us-east-1",
-    #       "eventTime": "1970-01-01T00:00:00.000Z",
-    #       "eventName": "ObjectCreated:Put",
-    #       "userIdentity": {
-    #         "principalId": "EXAMPLE"
-    #       },
-    #       "requestParameters": {
-    #         "sourceIPAddress": "127.0.0.1"
-    #       },
-    #       "responseElements": {
-    #         "x-amz-request-id": "EXAMPLE123456789",
-    #         "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
-    #       },
-    #       "s3": {
-    #         "s3SchemaVersion": "1.0",
-    #         "configurationId": "testConfigRule",
-    #         "bucket": {
-    #           "name": "example-bucket",
-    #           "ownerIdentity": {
-    #             "principalId": "EXAMPLE"
-    #           },
-    #           "arn": "arn:aws:s3:::example-bucket"
-    #         },
-    #         "object": {
-    #           "key": "test%2Fkey",
-    #           "size": 1024,
-    #           "eTag": "0123456789abcdef0123456789abcdef",
-    #           "sequencer": "0A1B2C3D4E5F678901"
-    #         }
-    #       }
-    #     }
-    #   ]
-    # }
+sqs = boto3.client("sqs", aws_region)
+s3 = boto3.client("s3", aws_region)
 
-    queue_url = os.getenv("TARGET_QUEUE_URL")
-    
+def lambda_handler(event: Dict[str, Any], context: Dict[Any, Any]):
+    # handle invalid events
     if not event or "Records" not in event:
         return {
             "StatusCode": HTTPStatus.BAD_REQUEST,
             "Message": "Event missing - Valid S3 event is required."
         }
-    
-    for record in event["Records"]:
-        if "s3" in record:
-            message_body = generate_message_payload(record)
 
-            response = send_sqs_message(queue_url, message_body)
+    # retrieve all target s3 objects and store it in target_objects array for further processing
+    target_objects: List[Dict[str, str]] = format_and_filter_targets(event)
 
-    return response
+    logger.info(target_objects)
 
-def send_sqs_message(queue_url: str, message_body: Dict[str, Any]):
-    try:
-        response = sqs.send_message(
-            QueueUrl=queue_url,
-            MessageBody=json.dumps(message_body)
-        )
-
-        logger.info(response)
-
-        return response
-
-    except Exception as e:
-        logger.error(f"Unexpected error had occurred: {e}")
-        raise
-
-def generate_message_payload(s3_record: Dict[str, Any]):
-    
-    bucket_name = s3_record["s3"]["bucket"]["name"]
-    object_key = s3_record["s3"]["object"]["key"]
-
-    message: Dict[str, Any] = {
-        "bucket_name": bucket_name,
-        "object_key": object_key
+    return {
+        "ResponseMetadata": {
+            "HTTPStatusCode": 200
+        }
     }
+
+def format_and_filter_targets(s3_event: Dict[str, Any]) -> List[Dict[str, str]]:
+    res = []
+    # iterate on s3 event records and append to res
+    for record in s3_event["Records"]:
+        bucket_name = record["s3"]["bucket"]["name"]
+        s3_object_key = record["s3"]["object"]["key"].split("/")
+        principal_id = record["userIdentity"]["principalId"]
+
+        prefix = "/".join(s3_object_key[:-1])
+        key = urllib.parse.unquote(s3_object_key[-1])
+
+        if "s3" in record and prefix == "batch/send" and key.endswith(".csv"):
+            res.append({
+                "bucket_name": bucket_name,
+                "prefix": prefix,
+                "key": key,
+                "principal_id": principal_id
+            })
+
+    return res
