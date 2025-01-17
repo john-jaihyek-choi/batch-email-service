6ad3beb40a3a519906ed3829b6e03b9e537945da
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-01-16 19:56:01 -0600

    optimization, logic update, return response structure update

--- cdk/lambdas/python/functions/send_batch_email_event/lambda_function.py
+++ cdk/lambdas/python/functions/send_batch_email_event/lambda_function.py
@@ -16,63 +16,79 @@ logging.basicConfig(level=os.getenv("LOG_LEVEL"))
 logger = logging.getLogger(__name__)
 
 aws_region = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
-recipients_per_message = os.getenv("RECIPIENTS_PER_MESSAGE", 50)
+recipients_per_message = int(os.getenv("RECIPIENTS_PER_MESSAGE", 50))
 queue_name = os.getenv("EMAIL_BATCH_QUEUE_NAME")
+csv_required_fields = os.getenv("CSV_REQUIRED_FIELDS")
 
 sqs = boto3.client("sqs", aws_region)
 s3 = boto3.client("s3", aws_region)
 
 
 def lambda_handler(event: Dict[str, Any], context: Dict[Any, Any]):
-    logger.debug("event: %s", json.dumps(event, indent=2))
+    logger.info("event: %s", event)
 
-    # handle invalid events
-    if not event or "Records" not in event:
+    if not event or "Records" not in event:  # handle invalid events
         logger.info("No s3 event records found")
 
         return {
-            "StatusCode": HTTPStatus.BAD_REQUEST,
+            "StatusCode": HTTPStatus.BAD_REQUEST.value,
             "Body": {"Message": "Event missing - Valid S3 event is required"},
             "Headers": {"Content-Type": "application/json"},
         }
 
     try:
-        # retrieve all target s3 objects and store it in target_objects array for further processing
-        target_objects: List[Dict[str, str]] = format_and_filter_targets(event)
+        target_objects: List[Dict[str, str]] = format_and_filter_targets(
+            event
+        )  # retrieve all target s3 objects and store it in target_objects array for further processing
 
         if not target_objects:
-            return {"StatusCode": 204, "Headers": {"Content-Type": "application/json"}}
+            return {
+                "StatusCode": HTTPStatus.NO_CONTENT.value,
+                "Headers": {"Content-Type": "application/json"},
+            }
 
-        total_batches_sent, errors = 0, 0
+        target_errors = []
 
         logger.info("successfully retrieved all targets from event")
         logger.debug("target_objects: %s", json.dumps(target_objects, indent=2))
 
-        # open csv target and organize by N batch
-        for target in target_objects:
+        for target in target_objects:  # open csv target and organize by N batch
             try:
-                process_targets(target)
-                total_batches_sent += 1
+                batch_errors = process_targets(target)
+
+                # add batch_errors to target_errors if any
+                if batch_errors.get("Errors"):
+                    target_errors.append(batch_errors)
+
             except Exception as e:
                 logger.error(f"Error processing target {target}: {e}")
-                errors += 1
+                target_errors.append({"Target": target, "Error": str(e)})
+                continue
 
-        logger.info("successfully processed the targets")
+        if target_errors:
+            logger.info("partially processed the batches")
+            return {
+                "StatusCode": HTTPStatus.PARTIAL_CONTENT.value,
+                "Body": {
+                    "Message": "Batch partially processed",
+                    "FailedBatches": target_errors,
+                },
+                "Headers": {"Content-Type": "application/json"},
+            }
+
+        logger.info("successfully processed the batches")
+
         return {
-            "StatusCode": HTTPStatus.OK,
+            "StatusCode": HTTPStatus.OK.value,
             "Body": {
                 "Message": "Batch processing completed successfully",
-                "processedRecords": len(target_objects),
-                "errors": errors,
-                "batchesSent": total_batches_sent,
-                "timestamp": datetime.now(timezone.utc).isoformat(),
             },
             "Headers": {"Content-Type": "application/json"},
         }
     except Exception as e:
-        logger.error(f"Unexpected error: {e}")
+        logger.error(f"Unexpected error: {str(e)}")
         return {
-            "StatusCode": HTTPStatus.INTERNAL_SERVER_ERROR,
+            "StatusCode": HTTPStatus.INTERNAL_SERVER_ERROR.value,
             "Body": {"Message": "An error occurred while processing the batch"},
             "Headers": {"Content-Type": "application/json"},
         }
@@ -82,15 +98,15 @@ def format_and_filter_targets(s3_event: Dict[str, Any]) -> List[Dict[str, str]]:
     logger.info("formatting and filtering tagets...")
 
     res = []
-    # iterate on s3 event records and append to res
-    for record in s3_event["Records"]:
+
+    for record in s3_event["Records"]:  # iterate on s3 event records and append to res
         event_type: str = record["eventName"]
         bucket_name: str = record["s3"]["bucket"]["name"]
         s3_object_key: str = record["s3"]["object"]["key"].split("/")
         principal_id: str = record["userIdentity"]["principalId"]
 
-        prefix = "/".join(s3_object_key[:-1])
-        key = urllib.parse.unquote(s3_object_key[-1])
+        prefix = "/".join(s3_object_key[:-1])  # get s3 object prefix
+        key = urllib.parse.unquote(s3_object_key[-1])  # get object key
 
         if (
             "s3" in record
@@ -100,122 +116,145 @@ def format_and_filter_targets(s3_event: Dict[str, Any]) -> List[Dict[str, str]]:
         ):
             res.append(
                 {
-                    "bucket_name": bucket_name,
-                    "prefix": prefix,
-                    "key": key,
-                    "principal_id": principal_id,
+                    "BucketName": bucket_name,
+                    "Prefix": prefix,
+                    "Key": key,
+                    "PrincipalId": principal_id,
+                    "Timestamp": datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S"),
                 }
             )
 
     return res
 
 
-def process_targets(s3_target: Dict[str, str]) -> None:
+def process_targets(s3_target: Dict[str, str]) -> Dict[str, Any]:
+    batch_errors = []
+
     try:
         logger.info("processing targets... %s", s3_target)
 
-        bucket_name, prefix, key, principal_id = (
-            s3_target["bucket_name"],
-            s3_target["prefix"],
-            s3_target["key"],
-            s3_target["principal_id"],
+        bucket_name, prefix, key, principal_id, timestamp = (
+            s3_target["BucketName"],
+            s3_target["Prefix"],
+            s3_target["Key"],
+            s3_target["PrincipalId"],
+            s3_target["Timestamp"],
         )
-        timestamp = datetime.now(timezone.utc)
 
-        logger.info(f"getting {bucket_name}/{prefix}/{key}...")
         s3_object = s3.get_object(Bucket=bucket_name, Key=f"{prefix}/{key}")
+        target_path = f"{bucket_name}/{prefix}/{key}"
 
-        logger.info(f"getting {bucket_name}/{prefix}/{key}...")
+        logger.info(f"getting {target_path}...")
         wrapper = io.TextIOWrapper(s3_object["Body"], encoding="utf-8")
 
         logger.info(f"grouping recipients by {recipients_per_message}...")
 
+        batch_number = 1
+
         # group the recipients and send message to sqs
-        batch_number = 0
-        for batch in batch_read_csv(wrapper, recipients_per_message):
+        for batch, failed_rows in batch_read_csv(wrapper, recipients_per_message):
+            batch_id = f"{bucket_name}/{prefix}/{key}-{timestamp}-{batch_number}"
             batch_number += 1
-            send_sqs_message(
-                bucket_name,
-                prefix,
-                key,
-                timestamp,
-                batch_number,
-                s3_target["principal_id"],
-                batch,
-            )
+
+            try:
+                if batch:
+                    send_sqs_message(
+                        batch_id,
+                        timestamp,
+                        batch_number,
+                        principal_id,
+                        batch,
+                    )
+            except Exception as e:
+                logger.error(f"Failed to send batch {batch_number}: {e}")
+                batch_errors.append(
+                    {
+                        "FailedRecipients": batch,
+                        "Error": f"Failed to send batch: {str(e)}",
+                    }
+                )
+                continue
+
+            if failed_rows:  # add to collection of failed rows to batch errors if any
+                batch_errors.extend(failed_rows)
 
     except Exception as e:
         logger.error(f"Unexpected error had occurred: {e}")
+        batch_errors.extend({"Error": str(e), "Target": s3_target})
+
+    return {
+        "Target": target_path,
+        "Errors": batch_errors,
+        "ErrorCount": len(batch_errors),
+    }
 
 
-# Generator to read a CSV file in batches.
-
-
+# reads a CSV file in batches.
 def batch_read_csv(file_obj, batch_size: int):
-    batch = []
+    batch, row_errors = [], []
     csv_reader = csv.DictReader(file_obj)
 
-    for row in csv_reader:
+    for row_number, row in enumerate(csv_reader, start=2):
         if not row:  # Skip empty rows
             continue
 
         try:
-            # validation logic goes here
+            validate_result = validate_row(row)
 
-            batch.append(row)
+            if validate_result["IsValid"]:
+                batch.append(row)
+            else:
+                row_errors.append(
+                    {
+                        "Error": "Missing required fields",
+                        "RowNumber": row_number,
+                        "MissingFields": validate_result["MissingFields"],
+                    }
+                )
         except Exception as e:
-            logger.error(f"Skipping malformed row: {row}. Error: {e}")
+            row_errors.append({"Error": "Unidentified error", "RowNumber": row_number})
             continue
 
         if len(batch) == batch_size:
-            yield batch
+            yield batch, None
             batch = []
 
-    if batch:  # Yield any remaining rows
-        yield batch
+    if batch or row_errors:  # Yield batch or row_errors if any
+        yield batch, row_errors
 
 
-def validate_row(row: Dict[str, Any]) -> bool:
-    # Validates a single CSV row.
+def validate_row(row: Dict[str, Any]) -> Dict[str, Any]:  # Validates a single CSV row.
+    missing_fields = []
 
-    required_fields = [
-        "send_to",
-        "first_name",
-        "last_name",
-        "send_from",
-        "email_template",
-    ]
-    for field in required_fields:
+    for field in csv_required_fields.split(","):
         if field not in row or not row[field]:
-            return False
+            missing_fields.append(field)
 
-    return True
+    return {"IsValid": not missing_fields, "MissingFields": missing_fields}
 
 
 def send_sqs_message(
-    bucket_name: str,
-    prefix: str,
-    key: str,
+    batch_id: str,
     timestamp: datetime,
     batch_number: int,
     principal_id: str,
     recipient_batch: List[Dict[str, Any]],
 ) -> Dict[Any, Any]:
     try:
-        aws_account_id = os.getenv("AWS_ACCOUNT_ID")
-
         logger.info("processing sqs message...")
 
         # organize message payload
         message = {
-            "batchId": f"{bucket_name}/{prefix}/{key}-{str(timestamp)}-{batch_number}",
-            "recipients": recipient_batch,
-            "metadata": {"uploadedBy": principal_id, "timestamp": str(timestamp)},
+            "BatchId": batch_id,
+            "Recipients": recipient_batch,
+            "Metadata": {"UploadedBy": principal_id, "Timestamp": timestamp},
         }
 
         logger.debug(f"sending batch {batch_number}...")
 
+        # get queue url
         queue_url = sqs.get_queue_url(QueueName=queue_name)["QueueUrl"]
+
         # send message to sqs queue
         response = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
 
