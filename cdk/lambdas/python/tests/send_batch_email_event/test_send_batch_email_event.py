8b46918d67c7634bde19f17c58c52ab772758e8b
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-01-20 18:50:16 -0600

    partial success test case added

--- cdk/lambdas/python/tests/send_batch_email_event/test_send_batch_email_event.py
+++ cdk/lambdas/python/tests/send_batch_email_event/test_send_batch_email_event.py
@@ -7,6 +7,7 @@ import boto3
 import os
 import logging
 import json
+from pathlib import Path
 from http import HTTPStatus
 from functions.send_batch_email_event.lambda_function import lambda_handler
 from aws_lambda_powertools.utilities.data_classes import S3Event
@@ -18,18 +19,25 @@ logger = logging.getLogger(__name__)
 # Test Cases
 @mock_aws
 @pytest.mark.parametrize(
-    "event_fixture, expected_message",
+    "valid_events, expected_message",
     [
         ("valid_single_record_event", "Batch processing completed successfully"),
         ("valid_multi_record_event", "Batch processing completed successfully"),
     ],
 )
-def test_valid_events(request, event_fixture, expected_message):
-    event = request.getfixturevalue(event_fixture)
+def test_valid_events(request, valid_events, expected_message):
+    event = request.getfixturevalue(valid_events)
     response = lambda_handler(event, {})
 
     assert response["StatusCode"] == HTTPStatus.OK
     assert response["Message"] == expected_message
+
+
+def test_partial_success(partial_success_event):
+    response = lambda_handler(partial_success_event, {})
+
+    assert response["StatusCode"] == HTTPStatus.PARTIAL_CONTENT
+    assert len(json.loads(response["Body"])["FailedBatches"][0]) > 0
 
 
 def test_missing_required_csv_fields(
@@ -37,8 +45,8 @@ def test_missing_required_csv_fields(
 ) -> None:
     response = lambda_handler(missing_required_csv_field_event, {})
 
-    assert response["StatusCode"] == HTTPStatus.PARTIAL_CONTENT
-    assert response["Message"] == "Batch partially processed"
+    assert response["StatusCode"] == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert response["Message"] == "Failed processing the batches"
     assert len(json.loads(response["Body"])["FailedBatches"][0]) > 0
 
 
@@ -100,6 +108,7 @@ def create_mock_s3():
         mock_recipients_data_path: List[str] = [
             "../../assets/batch/example-recipients-list-1.csv",
             "../../assets/batch/example-recipients-list-2.csv",
+            "../../assets/batch/example-recipients-list-3.csv",
             "../../assets/batch/bad-example-missing-required-column.csv",
         ]
 
@@ -159,6 +168,48 @@ def valid_single_record_event() -> S3Event:
 @pytest.fixture
 def valid_multi_record_event() -> S3Event:
     file_names = ["example-recipients-list-1.csv", "example-recipients-list-2.csv"]
+
+    payload = {"Records": []}
+
+    for name in file_names:
+
+        record = {
+            "eventVersion": "2.0",
+            "eventSource": "aws:s3",
+            "awsRegion": os.getenv("AWS_DEFAULT_REGION"),
+            "eventTime": "1970-01-01T00:00:00.000Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {"principalId": "EXAMPLE"},
+            "requestParameters": {"sourceIPAddress": "127.0.0.1"},
+            "responseElements": {
+                "x-amz-request-id": "EXAMPLE123456789",
+                "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH",
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "testConfigRule",
+                "bucket": {
+                    "name": os.getenv("TEST_MOCK_S3_BUCKET_NAME"),
+                    "ownerIdentity": {"principalId": "EXAMPLE"},
+                    "arn": f"arn:aws:s3:::{os.getenv("TEST_MOCK_S3_BUCKET_NAME")}",
+                },
+                "object": {
+                    "key": f"batch/send/{name}",
+                    "size": 1024,
+                    "eTag": "0123456789abcdef0123456789abcdef",
+                    "sequencer": "0A1B2C3D4E5F678901",
+                },
+            },
+        }
+
+        payload["Records"].append(record)
+
+    return payload
+
+
+@pytest.fixture
+def partial_success_event() -> S3Event:
+    file_names = ["example-recipients-list-3.csv"]
 
     payload = {"Records": []}
 
