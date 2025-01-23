2ba11bcb0ad8a7cbd5ea8b7b8917ce97e1a4bc39
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-01-23 13:24:29 -0600

    add more clear and robust error handling

--- cdk/lambdas/python/functions/send_batch_email_event/utils.py
+++ cdk/lambdas/python/functions/send_batch_email_event/utils.py
@@ -9,6 +9,7 @@ from datetime import datetime, timezone
 from pathlib import Path
 from dotenv import load_dotenv
 from typing import Dict, List, Any
+from boto3_helper import get_s3_object, send_sqs_message
 
 if Path(".env").exists():  # .env check for local execution
     load_dotenv()
@@ -17,14 +18,47 @@ logging.basicConfig(level=os.getenv("LOG_LEVEL"))
 logger = logging.getLogger(__name__)
 
 
-def generate_response(status_code: int, message: str, body: Dict[Any, Any] = None):
-    response = {
-        "StatusCode": status_code,
-        "Message": message,
-        "Header": {"Content-Type": "application/json"},
-        "Body": json.dumps(body),
-    }
-    return response
+# reads a CSV file in batches.
+def batch_read_csv(file_obj, batch_size: int):
+    batch, row_errors = [], []
+    csv_reader = csv.DictReader(file_obj)
+
+    for row_number, row in enumerate(csv_reader, start=2):
+        if not row:  # Skip empty rows
+            continue
+
+        try:
+            validate_result = validate_fields(row)
+
+            custom_fields = {"row_number": row_number}
+
+            row_info = OrderedDict({**custom_fields, **row})
+
+            if validate_result["IsValid"]:
+                batch.append(row_info)
+            else:
+                row_errors.append(
+                    OrderedDict(
+                        {
+                            **row_info,
+                            "Error": (
+                                "Missing required fields"
+                                if validate_result.get("MissingFields")
+                                else "Unidentified Error - Seek admin assistance"
+                            ),
+                            "MissingFields": validate_result.get("MissingFields"),
+                        }
+                    )
+                )
+        except Exception as e:
+            row_errors.append(OrderedDict({**row_info, "Error": "Unidentified error"}))
+            continue
+
+        if len(batch) == batch_size:
+            yield batch, []
+            batch = []
+    else:  # Yield batch and row_errors once completion
+        yield batch, row_errors
 
 
 def format_and_filter_targets(s3_event: Dict[str, Any]) -> List[Dict[str, str]]:
@@ -60,58 +94,6 @@ def format_and_filter_targets(s3_event: Dict[str, Any]) -> List[Dict[str, str]]:
     return res
 
 
-# reads a CSV file in batches.
-def batch_read_csv(file_obj, batch_size: int):
-    batch, row_errors = [], []
-    csv_reader = csv.DictReader(file_obj)
-
-    for row_number, row in enumerate(csv_reader, start=2):
-        if not row:  # Skip empty rows
-            continue
-
-        try:
-            validate_result = validate_row(row)
-
-            custom_fields = {"row_number": row_number}
-
-            row_info = OrderedDict({**custom_fields, **row})
-
-            if validate_result["IsValid"]:
-                batch.append(row_info)
-            else:
-                row_errors.append(
-                    OrderedDict(
-                        {
-                            **row_info,
-                            "Error": "Missing required fields",
-                            "MissingFields": validate_result["MissingFields"],
-                        }
-                    )
-                )
-        except Exception as e:
-            row_errors.append(OrderedDict({**row_info, "Error": "Unidentified error"}))
-            continue
-
-        if len(batch) == batch_size:
-            yield batch, None
-            batch = []
-
-    if batch or row_errors:  # Yield batch or row_errors if any
-        yield batch, row_errors
-
-
-def validate_row(row: Dict[str, Any]) -> Dict[str, Any]:  # Validates a single CSV row.
-    csv_required_fields = os.getenv("CSV_REQUIRED_FIELDS")
-
-    missing_fields = []
-
-    for field in csv_required_fields.split(","):
-        if field not in row or not row[field]:
-            missing_fields.append(field)
-
-    return {"IsValid": not missing_fields, "MissingFields": missing_fields}
-
-
 def generate_csv(headers: List[str], contents: List[Dict[str, Any]]) -> str:
     # Generate CSV content in memory
     try:
@@ -128,3 +110,156 @@ def generate_csv(headers: List[str], contents: List[Dict[str, Any]]) -> str:
         return csv_content
     except Exception as e:
         logger.error(f"Error generating csv: {e}")
+
+
+# def generate_html_template(
+#     s3_key: str,
+#     success_rate: int,
+# ) -> None:
+#     try:
+#         pass
+#     except Exception as e:
+#         logger.exception(f"Error generating html template: {e}")
+
+#     return
+
+
+def generate_response(status_code: int, message: str, body: Dict[Any, Any] = None):
+    try:
+        response = {
+            "StatusCode": status_code,
+            "Message": message,
+            "Header": {"Content-Type": "application/json"},
+            "Body": json.dumps(body),
+        }
+        return response
+    except TypeError as e:
+        logger.exception(f"Type error at generate_response: {e}")
+
+
+def generate_target_errors_payload(
+    target: str,
+    error_detail: str,
+    error_batch: List[Dict[str, Any]],
+    success_rate: int,
+) -> Dict[str, Any]:
+    try:
+        return {
+            "Target": target,
+            "Error": error_detail,
+            "Errors": error_batch,
+            "SuccessRate": success_rate,
+        }
+    except TypeError as e:
+        logger.exception(f"Type error at generate_target_errors_payload: {e}")
+
+
+def generate_batch_payload(
+    target_path: str, success_count: int, errors: List[Dict[str, Any]], error_count: int
+) -> Dict[str, Any]:
+    try:
+        return {
+            "Target": target_path,
+            "SuccessCount": success_count,
+            "Errors": errors,
+            "ErrorCount": error_count,
+        }
+    except TypeError as e:
+        logger.exception(f"Type error at generate_batch_payload: {e}")
+
+
+def process_targets(s3_target: Dict[str, str]) -> Dict[str, Any]:
+    try:
+        recipients_per_message = int(os.getenv("RECIPIENTS_PER_MESSAGE", 50))
+
+        batch_errors, success_count = [], 0
+
+        logger.debug("processing targets... %s", json.dumps(s3_target, indent=2))
+
+        bucket_name, prefix, key, principal_id, timestamp = (
+            s3_target["BucketName"],
+            s3_target["Prefix"],
+            s3_target["Key"],
+            s3_target["PrincipalId"],
+            s3_target["Timestamp"],
+        )
+
+        target_path = f"{bucket_name}/{prefix}/{key}"
+
+        logger.info(f"getting {target_path}...")
+        s3_object = get_s3_object(bucket_name=bucket_name, object_key=f"{prefix}/{key}")
+
+        if not s3_object.get("Body"):
+            logger.debug("No Body found in s3 object")
+            batch_errors.append("")
+            return generate_batch_payload(
+                target_path,
+                success_count,
+                [{"Error": f"No content found in {target_path}"}],
+                len(batch_errors),
+            )
+
+        wrapper = io.TextIOWrapper(s3_object.get("Body"), encoding="utf-8")
+
+        logger.info(f"grouping recipients by {recipients_per_message}...")
+
+        batch_number = 1
+
+        # group the recipients and send message to sqs
+        for batch, failed_rows in batch_read_csv(wrapper, recipients_per_message):
+            batch_id = f"{bucket_name}/{prefix}/{key}-{timestamp}-{batch_number}"
+            batch_number += 1
+            success_count += len(batch)
+
+            try:
+                if batch:
+                    send_sqs_message(
+                        batch_id,
+                        timestamp,
+                        batch_number,
+                        principal_id,
+                        batch,
+                    )
+            except Exception as e:
+                logger.exception(
+                    f"Failed to send sqs batch {batch_number} for {target_path}: {e}"
+                )
+                batch_errors.append(
+                    {
+                        "FailedRecipients": batch,
+                        "Error": f"Failed to send batch: {str(e)}",
+                    }
+                )
+        else:  # add to collection of failed rows when done
+            logger.debug("adding failed rows to batch_errors!")
+            batch_errors.extend(failed_rows)
+    except Exception as e:
+        logger.exception(f"Error at processing target: {e}")
+        batch_errors.append({"Error": "Unexpected error", "Details": str(e)})
+    finally:
+        return {
+            "Target": target_path,
+            "SuccessCount": success_count,
+            "Errors": batch_errors,
+            "ErrorCount": len(batch_errors),
+        }
+
+
+def validate_fields(
+    row: Dict[str, Any]
+) -> Dict[str, Any]:  # Validates a single CSV row.
+    try:
+        csv_required_fields = os.getenv("CSV_REQUIRED_FIELDS")
+
+        missing_fields = []
+
+        for field in csv_required_fields.split(","):
+            if field not in row or not row[field]:
+                missing_fields.append(field)
+
+        return {"IsValid": not missing_fields, "MissingFields": missing_fields}
+    except Exception as e:
+        logger.exception(f"Error validating row: {e}")
+        return {
+            "IsValid": False,
+        }
