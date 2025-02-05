35662cd0ad228109bb9073bf9ade7520a8a670c4
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-02-04 23:55:58 -0600

    config typing update for send batch email event

--- cdk/lambdas/python/send_batch_email_event/config.py
+++ cdk/lambdas/python/send_batch_email_event/config.py
@@ -1,32 +1,34 @@
 import os
 import logging
-from typing import List
+from typing import List, Optional
 
 logger = logging.getLogger(__name__)
 logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 
 
 class Config:
-    EMAIL_BATCH_QUEUE_NAME: str = os.getenv("EMAIL_BATCH_QUEUE_NAME")
-    DLQ_QUEUE_NAME: str = os.getenv("DLQ_QUEUE_NAME")
+    EMAIL_BATCH_QUEUE_NAME: str = os.getenv("EMAIL_BATCH_QUEUE_NAME", "")
+    DLQ_QUEUE_NAME: str = os.getenv("DLQ_QUEUE_NAME", "")
     RECIPIENTS_PER_MESSAGE: int = int(os.getenv("RECIPIENTS_PER_MESSAGE", 50))
     LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
     AWS_DEFAULT_REGION: str = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
-    SES_NO_REPLY_SENDER: str = os.getenv("SES_NO_REPLY_SENDER")
-    SES_ADMIN_EMAIL: str = os.getenv("SES_ADMIN_EMAIL")
-    BATCH_EMAIL_SERVICE_BUCKET_NAME: str = os.getenv("BATCH_EMAIL_SERVICE_BUCKET_NAME")
+    SES_NO_REPLY_SENDER: str = os.getenv("SES_NO_REPLY_SENDER", "")
+    SES_ADMIN_EMAIL: str = os.getenv("SES_ADMIN_EMAIL", "")
+    BATCH_EMAIL_SERVICE_BUCKET_NAME: str = os.getenv(
+        "BATCH_EMAIL_SERVICE_BUCKET_NAME", ""
+    )
     SEND_BATCH_EMAIL_FAILURE_HTML_TEMPLATE_KEY: str = os.getenv(
-        "SEND_BATCH_EMAIL_FAILURE_HTML_TEMPLATE_KEY"
+        "SEND_BATCH_EMAIL_FAILURE_HTML_TEMPLATE_KEY", ""
     )
     SEND_BATCH_EMAIL_FAILURE_TEXT_TEMPLATE_KEY: str = os.getenv(
-        "SEND_BATCH_EMAIL_FAILURE_TEXT_TEMPLATE_KEY"
+        "SEND_BATCH_EMAIL_FAILURE_TEXT_TEMPLATE_KEY", ""
     )
     BATCH_INITIATION_ERROR_S3_PREFIX: str = os.getenv(
-        "BATCH_INITIATION_ERROR_S3_PREFIX"
+        "BATCH_INITIATION_ERROR_S3_PREFIX", ""
     )
-    BATCH_PROCESS_ERROR_S3_PREFIX: str = os.getenv("BATCH_PROCESS_ERROR_S3_PREFIX")
+    BATCH_PROCESS_ERROR_S3_PREFIX: str = os.getenv("BATCH_PROCESS_ERROR_S3_PREFIX", "")
     EMAIL_REQUIRED_FIELDS: List[str] = os.getenv("EMAIL_REQUIRED_FIELDS", "").split(",")
-    TEMPLATE_METADATA_TABLE_NAME: str = os.getenv("TEMPLATE_METADATA_TABLE_NAME")
+    TEMPLATE_METADATA_TABLE_NAME: str = os.getenv("TEMPLATE_METADATA_TABLE_NAME", "")
 
 
 config = Config()
