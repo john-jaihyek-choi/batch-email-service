f30c05a3f0ab599f45f9ee729ed30b3f5645628c
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-02-05 19:01:44 -0600

    prefix format update

--- cdk/lambdas/python/tests/send_batch_email_event/test_send_batch_email_event.py
+++ cdk/lambdas/python/tests/send_batch_email_event/test_send_batch_email_event.py
@@ -567,7 +567,7 @@ def failed_s3_object_moved_successfully(
         # check destination object
         try:
             new_destination_key = (
-                f"{os.getenv("BATCH_INITIATION_ERROR_S3_PREFIX")}/{object}"
+                f"{os.getenv("BATCH_INITIATION_ERROR_S3_PREFIX")}{object}"
             )
             s3.get_object(Bucket=bucket, Key=new_destination_key)
             logger.info(
