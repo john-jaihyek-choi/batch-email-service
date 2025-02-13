6986fa3115e820ba2335a3c15cd49394d7f919c1
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-02-13 17:23:07 -0600

    code refactor as a result of singleton pattern change in aws client

--- cdk/lambdas/python/tests/conftest.py
+++ cdk/lambdas/python/tests/conftest.py
@@ -12,6 +12,7 @@ from moto import mock_aws
 from mypy_boto3_sqs.client import SQSClient
 from mypy_boto3_s3.client import S3Client
 from mypy_boto3_ses.client import SESClient
+from mypy_boto3_sesv2.client import SESV2Client
 from mypy_boto3_dynamodb.client import DynamoDBClient
 from mypy_boto3_s3.literals import BucketLocationConstraintType
 from mypy_boto3_dynamodb.type_defs import WriteRequestUnionTypeDef
@@ -19,16 +20,19 @@ from dotenv import load_dotenv
 
 # local modules
 from tests.types import S3EventRecordPayload
+from jc_custom.boto3_helper import aws_client
 
 load_dotenv()
 
 logger = logging.getLogger(__name__)
 
+aws_region = os.getenv("AWS_DEFAULT_REGION", "us-east-2")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def logger_setup():
     # Restrict external library logs to WARNING due to noise
-    hide_logs = os.getenv("HIDE_LOGS_LIST", "").split(",")
+    hide_logs = ["boto3_helper", "boto3", "urlib3", "botocore", "s3transfer"]
     for module in hide_logs:
         logging.getLogger(module).setLevel(logging.WARNING)
 
@@ -37,112 +41,110 @@ def logger_setup():
 
 
 @pytest.fixture(scope="module", autouse=True)
-def mocked_sqs() -> Generator[SQSClient, None, None]:
+def mocked_aws():
     with mock_aws():
-        try:
-            sqs: SQSClient = boto3.client("sqs", os.getenv("AWS_DEFAULT_REGION"))
-            sqs.create_queue(QueueName=os.getenv("EMAIL_BATCH_QUEUE_NAME", ""))
-        except Exception as e:
-            pytest.fail(f"Failed setting up mock sqs {e}")
-        yield sqs
+        logger.warning("Starting mock_aws session...")
+        # Any pre-load configurations if needed
+        yield
 
 
 @pytest.fixture(scope="module", autouse=True)
-def mocked_s3() -> Generator[S3Client, None, None]:
+def mocked_sqs(mocked_aws) -> Generator[SQSClient, None, None]:
+    try:
+        sqs: SQSClient = aws_client.get_client("sqs", aws_region)
+        sqs.create_queue(QueueName=os.getenv("EMAIL_BATCH_QUEUE_NAME", ""))
+
+    except Exception as e:
+        pytest.fail(f"Failed setting up mock sqs {e}")
+    yield sqs
+
+
+@pytest.fixture(scope="module", autouse=True)
+def mocked_s3(mocked_aws) -> Generator[S3Client, None, None]:
     aws_region = cast(
         BucketLocationConstraintType, os.getenv("AWS_DEFAULT_REGION", "us-east-2")
     )
     bucket_name: str = os.getenv("BATCH_EMAIL_SERVICE_BUCKET_NAME", "")
 
-    with mock_aws():
-        try:
-            s3: S3Client = boto3.client("s3", aws_region)
-            s3.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={
-                    "LocationConstraint": aws_region,
-                },
+    try:
+        s3: S3Client = aws_client.get_client("s3", aws_region)
+        s3.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={
+                "LocationConstraint": aws_region,
+            },
+        )
+
+        test_assets = [
+            {
+                "local_path": os.getenv("TEST_EXAMPLE_BATCH_PATH", ""),
+                "s3_prefix": "batch/send/",
+            },
+            {
+                "local_path": os.getenv("TEST_EXAMPLE_TEMPLATE_PATH", ""),
+                "s3_prefix": "templates/",
+            },
+        ]
+
+        for asset in test_assets:
+            upload_directory_to_mocked_s3(
+                s3=s3,
+                bucket_name=bucket_name,
+                local_path=asset["local_path"],
+                s3_prefix=asset["s3_prefix"],
             )
 
-            test_assets = [
-                {
-                    "local_path": os.getenv("TEST_EXAMPLE_BATCH_PATH", ""),
-                    "s3_prefix": "batch/send/",
-                },
-                {
-                    "local_path": os.getenv("TEST_EXAMPLE_TEMPLATE_PATH", ""),
-                    "s3_prefix": "templates/",
-                },
-            ]
+    except Exception as e:
+        pytest.fail(f"Failed setting up mock s3: {e}")
 
-            for asset in test_assets:
-                upload_directory_to_mocked_s3(
-                    s3=s3,
-                    bucket_name=bucket_name,
-                    local_path=asset["local_path"],
-                    s3_prefix=asset["s3_prefix"],
-                )
-
-        except Exception as e:
-            pytest.fail(f"Failed setting up mock s3: {e}")
-
-        yield s3
+    yield s3
 
 
 @pytest.fixture(scope="module", autouse=True)
-def mocked_ses() -> Generator[SESClient, None, None]:
-    # Initialize SES client
-    with mock_aws():
-        try:
-            ses: SESClient = boto3.client(
-                "ses", region_name=os.getenv("AWS_DEFAULT_REGION")
-            )
-            ses.verify_email_identity(EmailAddress=os.getenv("SES_NO_REPLY_SENDER", ""))
-        except Exception as e:
-            pytest.fail(f"Failed to setup ses client and/or verify email identity: {e}")
-        yield ses
+def mocked_ses(mocked_aws) -> Generator[SESV2Client, None, None]:
+    try:
+        sesv2: SESV2Client = aws_client.get_client("sesv2", aws_region)
+        ses: SESClient = aws_client.get_client("ses", aws_region)
+        ses.verify_email_identity(EmailAddress=os.getenv("SES_NO_REPLY_SENDER", ""))
+    except Exception as e:
+        pytest.fail(f"Failed to setup ses client and/or verify email identity: {e}")
+    yield sesv2
 
 
 @pytest.fixture(scope="module", autouse=True)
-def mocked_ddb() -> Generator[DynamoDBClient, None, None]:
-    # Create table and put items
+def mocked_ddb(mocked_aws) -> Generator[DynamoDBClient, None, None]:
     db_path: str = os.getenv("TEST_EXAMPLE_DB_PATH", "")
-    logger.warning(db_path)
-    with mock_aws():
-        try:
-            table_name = os.getenv("TEMPLATE_METADATA_TABLE_NAME", "")
+    try:
+        table_name = os.getenv("TEMPLATE_METADATA_TABLE_NAME", "")
+        ddb: DynamoDBClient = aws_client.get_client("dynamodb", aws_region)
 
-            ddb: DynamoDBClient = boto3.client(
-                "dynamodb", region_name=os.getenv("AWS_DEFAULT_REGION")
-            )
+        ddb.create_table(
+            TableName=table_name,
+            AttributeDefinitions=[
+                {"AttributeName": "template_key", "AttributeType": "S"},
+            ],
+            KeySchema=[{"AttributeName": "template_key", "KeyType": "HASH"}],
+            BillingMode="PAY_PER_REQUEST",
+        )
 
-            ddb.create_table(
-                TableName=table_name,
-                AttributeDefinitions=[
-                    {"AttributeName": "template_key", "AttributeType": "S"},
-                ],
-                KeySchema=[{"AttributeName": "template_key", "KeyType": "HASH"}],
-                BillingMode="PAY_PER_REQUEST",
-            )
+        ddb.get_waiter("table_exists").wait(TableName=table_name)
 
-            ddb.get_waiter("table_exists").wait(TableName=table_name)
+        batch_write_item: List[WriteRequestUnionTypeDef] = []
 
-            batch_write_item: List[WriteRequestUnionTypeDef] = []
+        with open(db_path) as file:
+            rows = json.load(file)
 
-            with open(db_path) as file:
-                rows = json.load(file)
+            for row in rows:
+                batch_write_item.append({"PutRequest": {"Item": row}})
 
-                for row in rows:
-                    batch_write_item.append({"PutRequest": {"Item": row}})
+        ddb.batch_write_item(
+            RequestItems={table_name: batch_write_item},
+            ReturnConsumedCapacity="TOTAL",
+        )
 
-            ddb.batch_write_item(
-                RequestItems={table_name: batch_write_item},
-                ReturnConsumedCapacity="TOTAL",
-            )
-
-        except Exception as e:
-            pytest.fail(f"Failed to setup mock ddb: {e}")
-        yield ddb
+    except Exception as e:
+        pytest.fail(f"Failed to setup mock ddb: {e}")
+    yield ddb
 
 
 @pytest.fixture(scope="module")
