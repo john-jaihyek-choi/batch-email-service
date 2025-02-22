bb44710dcddb660167ad1b342273ef220632319e
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-02-21 18:39:05 -0600

    basic lambda handler with basic logic implemented

--- cdk/lambdas/python/process_batch_email_event/main.py
+++ cdk/lambdas/python/process_batch_email_event/main.py
@@ -1,1 +1,53 @@
-# placeholder
+# stdlib
+import json
+import logging
+from typing import Dict, Any, Optional
+from http import HTTPStatus
+
+# external libraries
+from aws_lambda_powertools.utilities.data_classes import SQSEvent
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+# custom module
+from jc_custom.utils import (
+    generate_handler_response,
+    filter_sqs_event,
+)
+from process_batch_email_event_config import config
+from process_batch_email_event_processor import process_sqs_message_targets
+
+logger = logging.getLogger(__name__)
+logger.setLevel(config.LOG_LEVEL)
+
+
+def lambda_handler(
+    event: SQSEvent, context: Optional[LambdaContext] = None
+) -> Dict[str, Any]:
+    logger.info("event: %s", json.dumps(event, indent=2))
+
+    try:
+        if not event or not event.get("Records"):
+            raise ValueError("Invalid event: Missing 'Records' key")
+
+        target_messages = filter_sqs_event(event)
+
+        if not target_messages:
+            return generate_handler_response(
+                status_code=HTTPStatus.NO_CONTENT.value,
+                message="No valid message targets found",
+            )
+
+        response = process_sqs_message_targets(target_messages)
+
+        return response
+
+    except ValueError as e:
+        logger.exception(f"Value Error: {e}")
+        return generate_handler_response(HTTPStatus.BAD_REQUEST.value, str(e))
+
+    except Exception as e:
+        logger.exception(f"Critical error in lambda_handler: {str(e)}")
+        return generate_handler_response(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+            message="An error occurred while processing the batch",
+        )
